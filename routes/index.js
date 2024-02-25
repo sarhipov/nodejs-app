f80e43050e804043b0b21c9dac3c3eae6d18449c
Sergei Arhipov <sergei.arhipov@topia.com>
2024-02-25 17:50:15 +0200

    add timestamp

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var os = require('os');
+const { format } = require('date-fns');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const currentTimeLocal = format(new Date(), 'dd-MM-yyyy HH:mm:ss');
+
   res.send({
     hostname: os.hostname(),
     version: process.env.npm_package_version,
+    timestampLocal: currentTimeLocal,
   });
 });
 
